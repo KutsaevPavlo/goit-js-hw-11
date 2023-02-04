32d2cf5288d612a005ae953bcef23f3feab74324
Pavlo <p1kutsaev@gmail.com>
2023-02-04 19:29:04 +0200

    update 11

--- src/Axio.js
+++ src/Axio.js
@@ -8,11 +8,11 @@ const axios = require('axios');
 
 
 function AxioSearch(name){
-return axios.get(`${ENDPOINT}${MY_API_KEY}&q${name}&image_type=photo&orientation=horizontal&safesearch=true`)
+return axios.get(`${ENDPOINT}${MY_API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`)
 .then((response) => {
-    // if(!response.ok){
-    //     throw new Error(response.statusText);
-    // }
+    if(response.data.totalHits === 0){
+        throw new Error(response.statusText);
+    }
     return response.data
 });
 }
--- src/index.js
+++ src/index.js
@@ -1,63 +1,65 @@
 // // import './css/styles.css';
 import API from "./Axio.js"
-// import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 
 
 // const DEBOUNCE_DELAY = 300;
 // const _ = require('lodash');
 // const countryList = document.querySelector(".country-list");
 // const countryInfo = document.querySelector(".country-info");
-// const form = document.getElementById("search-box");
+const form = document.getElementById("search-form");
 
-// form.addEventListener("input", _.debounce(onInput, DEBOUNCE_DELAY));
+form.addEventListener("submit", onInput);
 
-// function onInput(e){
-//     e.preventDefault();
-//     let inputValue = e.target.value.trim();
+function onInput(e){
+    e.preventDefault();
+    
+    const inputValue = e.currentTarget.elements.searchQuery.value;
+    console.log(inputValue);
 
-//     cleanerMarkup(countryList);
-//     cleanerMarkup(countryInfo);
+    // cleanerMarkup(countryList);
+    // cleanerMarkup(countryInfo);
 
-//     if(inputValue === ''){
-//         return 
-//     };   
+    if(inputValue === ''){
+        return 
+    };   
 
-//     API.fetchCountries(inputValue).then((data) => {
+    API.AxioSearch(inputValue).then((data) => {
         
-//         const markupForMoreThenTwo= createMarkupForMoreThenTwo(data);
-//         const markupForOne= createMarkup(data);
+        const markupForMoreThenTwo= createMarkupForMoreThenTwo(data);
+        const markupForOne= createMarkup(data);
         
-//         console.log(data);
+        console.log(data);
         
         
-//         if (data.length > 10) {
-//         Notiflix.Notify.info("Too many matches found. Please enter a more specific name")} 
+        if (data.length > 10) {
+        Notiflix.Notify.info("Too many matches found. Please enter a more specific name")} 
             
-//         else if(data.length >= 2 && data.length < 10) {
-//         addMarkup(countryList, markupForMoreThenTwo);}
+        else if(data.length >= 2 && data.length < 10) {
+        addMarkup(countryList, markupForMoreThenTwo);}
                 
-//         else 
-//         addMarkup(countryInfo, markupForOne);
-//         })
+        else 
+        addMarkup(countryInfo, markupForOne);
+        })
         
-//         .catch(error => Notiflix.Notify.failure("Oops, there is no country with that name"));
+        .catch(error => Notiflix.Notify.failure("Oops, there is no country with that name"));
             
-//         };
+        };
         
-//         function createMarkup(data){
-//             return data
-//         .map(({ name, capital, population, flags, languages}) => {
-//             return `
-//             <div class="countrie">
-//             <h2 class="countrie-card-titel"> <img src="${flags.svg}" alt="" width="70" height="50"> ${name.official}</h2>
-//             <p>Capital: ${capital}</p>
-//             <p>Population: ${population}</p>
-//             <p>Languages: ${Object.values(languages)}</p>
-//             </div>
-//             `
-//         })
-//         .join("");
-//     };
+        function createMarkup(data){
+            return data
+        .map(({ name, capital, population, flags, languages}) => {
+            return `
+            <div class="countrie">
+            <h2 class="countrie-card-titel"> <img src="${flags.svg}" alt="" width="70" height="50"> ${name.official}</h2>
+            <p>Capital: ${capital}</p>
+            <p>Population: ${population}</p>
+            <p>Languages: ${Object.values(languages)}</p>
+            </div>
+            `
+        })
+        .join("");
+    };
     
 //  function createMarkupForMoreThenTwo(data){
 //         return data
