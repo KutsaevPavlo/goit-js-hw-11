db58f584c25f58bf54239d926d7794563004fd1f
Pavlo <p1kutsaev@gmail.com>
2023-02-06 10:16:12 +0200

    Update Axio.js

--- src/Axio.js
+++ src/Axio.js
@@ -11,26 +11,51 @@ export default class ImgApi{
         this.queryPage = 1;
         this.searchQuery = "";
         // this.countImg = 0;
-
-        
+    
     }
     
-    async AxioSearch(){
-        return await axios.get(`${ENDPOINT}${MY_API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.queryPage}`)
-    .then((response) => {
-        if(response.data.totalHits === 0){
-            throw new Error(response.statusText);
-        }
+    async AxioSearch() {
+        const BASE_URL = 'https://pixabay.com/api/';
+    
+        const searchParams = new URLSearchParams({
+          key: `33373070-0a3de92214998aff69d545527`,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: 40,
+        });
+    
+        const response = await axios.get(
+          `${BASE_URL}?${searchParams}&q=${this.searchQuery}&page=${this.queryPage}`
+        );
+    
+        return response.data;
+      }
+
+
+
+
+
+
+
+
+
+    // async AxioSearch(){
+    //     return await axios.get(`${ENDPOINT}${MY_API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.queryPage}`)
+    // .then((response) => {
+    //     if(response.data.totalHits === 0){
+    //         throw new Error(response.statusText);
+    //     }
         
-        console.log(response.data.totalHits);
-        return response.data
-    }).then((data => {
-        this.incrementPage();
+    //     console.log(response.data.totalHits);
+    //     return response.data
+    // }).then((data => {
+    //     this.incrementPage();
       
         
-        return data;
-    }));
-    }
+    //     return data;
+    // }));
+    // }
 
     resetPage(){
         this.queryPage = 1;
