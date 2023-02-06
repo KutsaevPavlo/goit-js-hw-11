b7be6ca26140fb263a1de116bf8deae7acf464f4
Pavlo <p1kutsaev@gmail.com>
2023-02-06 11:47:30 +0200

    Update Axio.js

--- src/Axio.js
+++ src/Axio.js
@@ -14,23 +14,23 @@ export default class ImgApi{
     
     }
     
-    async AxioSearch() {
-        const BASE_URL = 'https://pixabay.com/api/';
+    // async AxioSearch() {
+    //     const BASE_URL = 'https://pixabay.com/api/';
     
-        const searchParams = new URLSearchParams({
-          key: `33373070-0a3de92214998aff69d545527`,
-          image_type: 'photo',
-          orientation: 'horizontal',
-          safesearch: true,
-          per_page: 40,
-        });
+    //     const searchParams = new URLSearchParams({
+    //       key: `33373070-0a3de92214998aff69d545527`,
+    //       image_type: 'photo',
+    //       orientation: 'horizontal',
+    //       safesearch: true,
+    //       per_page: 40,
+    //     });
     
-        const response = await axios.get(
-          `${BASE_URL}?${searchParams}&q=${this.searchQuery}&page=${this.queryPage}`
-        );
+    //     const response = await axios.get(
+    //       `${BASE_URL}?${searchParams}&q=${this.searchQuery}&page=${this.queryPage}`
+    //     );
     
-        return response.data;
-      }
+    //     return response.data;
+    //   }
 
 
 
@@ -40,22 +40,22 @@ export default class ImgApi{
 
 
 
-    // async AxioSearch(){
-    //     return await axios.get(`${ENDPOINT}${MY_API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.queryPage}`)
-    // .then((response) => {
-    //     if(response.data.totalHits === 0){
-    //         throw new Error(response.statusText);
-    //     }
+    async AxioSearch(){
+        return await axios.get(`${ENDPOINT}${MY_API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.queryPage}`)
+    .then((response) => {
+        if(response.data.totalHits === 0){
+            throw new Error(response.statusText);
+        }
         
-    //     console.log(response.data.totalHits);
-    //     return response.data
-    // }).then((data => {
-    //     this.incrementPage();
+        console.log(response.data.totalHits);
+        return response.data
+    }).then((data => {
+        this.incrementPage();
       
         
-    //     return data;
-    // }));
-    // }
+        return data;
+    }));
+    }
 
     resetPage(){
         this.queryPage = 1;
